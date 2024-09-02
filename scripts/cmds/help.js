dd5ada642bd684fa7b07bf4fd266ac767cb20ada
lhw-ren <yeshuasynovic@gmail.com>
2024-09-02 17:30:22 +0800

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -1,15 +1,3 @@
-const fs = require("fs-extra");
-const axios = require("axios");
-const path = require("path");
-const { getPrefix } = global.utils;
-const { commands, aliases } = global.GoatBot;
-const doNotDelete = "[ 🐐 | Goat Bot V2 ]";
-/** 
-* @author NTKhang
-* @author: do not delete it
-* @message if you delete or edit it you will get a global ban
-*/
-
 module.exports = {
     config: {
         name: "help",
@@ -19,6 +7,6 @@ module.exports = {
         guide: "{pn}"
     },
     onStart: async function ({ message }) {
-        message.reply("I'm RTUMM-GAND AI,\nyour new AI buddy by Math Major.\nJust type my prefix (#) to ask me anything.\n🎀———————————🎀\nLIST OF BOT'S COMMANDS\n🎀—————————————🎀\n#help [command name]\n#math [calculate]\n#calculate [provide an expression to calculate]\n#ai [query]\n#gpt, gpt2, gpt3 [gpt]\n#dictionary [lookup at dictionary]\n#define [define it]\n#quiz [subject]\n#guessnumber [number game]\n#google [query]\n#imgsearch [search image]\n🎀—————————🎀\ntype #help [command name] to see description and usage of commands.");
+        message.reply("I'm RTUMM-GAND AI, your new AI buddy by Math Major. Just type my prefix (#) to ask me anything.\n🎀————————————🎀\nLIST OF BOT'S COMMANDS\n🎀————————————🎀\n#help [command name]\n#math [calculate]\n#calculate [provide an expression to calculate]\n#ai [query]\n#gpt3 [query]\n#define [word]\n#dictionary [lookup at dictionary]\n#quiz [subject]\n#guessnumber [number game]\n#google [query]\n#imgsearch [search image]\n🎀—————————🎀\ntype #help <command name> to see description and usage of commands.");
     }
 };
