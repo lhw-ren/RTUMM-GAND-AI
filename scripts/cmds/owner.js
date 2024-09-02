96456befb690979b0c077c871b009d6a3d237ab2
lhw-ren <yeshuasynovic@gmail.com>
2024-09-02 14:14:02 +0800

    Update owner.js

--- scripts/cmds/owner.js
+++ scripts/cmds/owner.js
@@ -7,7 +7,7 @@ module.exports = {
         longDescription: "See owner name and link"
     },
     onStart: async function ({ message, api, event }) {
-        const msg = "Name: Micazhla Javhiuor\nAge: 15\nHobby: Coding\nStatus: Single\nLink: https://www.facebook.com/profile.php?id=61557494398506";
+        const msg = "Name: Daryl\nAge: 20\nHobby: Watching Series\nStatus: Single\nLink: https://www.facebook.com/daryl.abarracoso\nIgLink:https://www.instagram.com/hhjryl?igsh=MWRodDE2b3I5OWtjdg==";
         
         message.reply(msg);
     }
