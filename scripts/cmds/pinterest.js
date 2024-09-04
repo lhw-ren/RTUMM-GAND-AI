b4770048018ab61abd9543bd86c2c3069620192c
lhw-ren <yeshuasynovic@gmail.com>
2024-09-04 09:58:38 +0800

    Update pinterest.js

--- scripts/cmds/pinterest.js
+++ scripts/cmds/pinterest.js
@@ -1,7 +1,7 @@
-const axios = require('axios');
-const fs = require("fs");
-
-const cooldowns = {}; 
+const axios = require("axios");
+const fs = require("fs-extra");
+const request = require("request");
+const path = require("path");
 
 module.exports.config = {
   name: "pin",
